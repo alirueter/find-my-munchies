985cbfbd976c18e10c2711b7f04bbeeeea151b4b
Sforeman270 <65334884+Sforeman270@users.noreply.github.com>
2020-07-11 18:58:24 -0500

    initial generate html/js

---
+++ assets/app.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const randomBtn = document.querySelector('#generate-btn')
+    const randomBeer = document.querySelector('.random-beer')
+    const descriptionDisplay = document.querySelector('.description')
+    const foodPairingDisplay = document.querySelector('.food_pairing')
+
+
+
+    function getData() {
+        e.preventDefault()
+        fetch('https://api.punkapi.com/v2/beers/random')
+            .then(response => {
+                return response.json()
+
+            })
+            .then(data => {
+                const name = data[0].name
+                const description = data[0].description
+                const ibu = data[0].ibu
+                const abv = data[0].abv
+                const foodPairing = data[0].food_pairing
+
+
+                randomBeer.innerHTML = name + '' + ibu + '' + abv
+                descriptionDisplay.innerHTML = description
+                foodPairingDisplay.innerHTML = food_pairing
+
+            })
+    
+
+    randomBtn.addEventListener('click', getData)
+        }
+
+})
