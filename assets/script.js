df88eaf9dae53f8daa30a80ce5240e98dd5f70b9
Taryn Fanning <taryn.fanning@gmail.com>
2020-07-14 15:01:58 -0500

    food/drink html and beginning js

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+var foodInput = document.querySelector(".food-input")
+var foodSearchBtn = document.querySelector(".food-search")
+var drinkInput = document.querySelector(".drink-input")
+var drinkSearchBtn = document.querySelector(".drink-search")
+
+//Details of search
+var foodLocation = document.querySelector(".location-food")
+var cuisine = document.querySelector(".cuisine")
+var meal = document.querySelector(".meal")
+var drinkLocation = document.querySelector(".location-drink")
+var beerType = document.querySelector(".beer-type")
+var beer = document.querySelector(".beer")
+
+foodSearchBtn.on("click", function(e) {
+    e.preventDefault();
+    if (foodInput.val() === "") {
+        
+    }
+})
